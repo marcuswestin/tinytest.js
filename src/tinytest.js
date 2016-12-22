a45a87d3cc5f72b0ed41b28eba734278b34d8908
Marcus Westin <marcus.westin@gmail.com>
2016-12-21 19:19:19 -0500

    Add ability to pass options to runTests; exit with an error code if there was a failed test or no tests were found; Add failFast option to fail right away in case of a test error; Print a warning if there were skipped tests (when using failFast option); Handle errors thrown during test setup; Add test.group() function for prefixing test names - this will also allow for per-group configurations later (e.g test setup, teardown, etc)

--- src/tinytest.js
+++ src/tinytest.js
@@ -5,39 +5,68 @@ var tinytest = module.exports = {
 	test: test,
 	assert: assert,
 	print: print,
-	noConflict: noConflict,
-	
-	outputEl: null,
-	maxDuration: 150
+	noConflict: noConflict
 }
 
-function runTests() {
+function runTests(_opts) {
+	assign(opts, _opts)
 	runner.t0 = new Date()
 	runner._runNextTest()
 }
 
-function test(name, fn) {
-	runner.tests.push({ name:name, fn:fn })
+function assign(obj, props1, props2, etc) {
+	if (!obj) { obj = {} }
+	for (var i=1; i<arguments.length; i++) {
+		var props = arguments[i]
+		for (var key in props) {
+			if (props.hasOwnProperty(key)) {
+				obj[key] = props[key]
+			}
+		}
+	}
+	return obj
 }
 
-function assert(ok, msg) {
+test.group = function(groupName, fn) {
+	var resetGroupName = runner.currentGroup
+	runner.currentGroup = groupName
+	fn()
+	runner.currentGroup = resetGroupName
+}
+
+function test(name, fn) {
+	runner.tests.push({ name:runner.currentGroup+' - '+name, fn:fn })
+}
+
+function assert(ok, msg1, msg2, etc) {
 	if (!ok) {
+		var msg = Array.prototype.slice.call(arguments, 1).join(' ')
+		debugger
 		throw new Error('assert failed' + (msg ? ': ' + msg : ''))
 	}
 }
 
 function print() {
 	var args = Array.prototype.slice.call(arguments, 0)
-	var msg = args.join(' ')
-	console.log.apply(console, arguments)
+	
+	// First, print into browser DOM
 	if (isBrowser) {
-		if (!tinytest.outputEl) {
-			tinytest.outputEl = document.createElement('div')
-			tinytest.outputEl.style = 'font-family:monaco,sans-serif; font-size:12px; padding:10px; background-color:black; color:white;'
-			document.body.appendChild(tinytest.outputEl)
+		if (!opts.outputEl) {
+			opts.outputEl = document.createElement('div')
+			opts.outputEl.style = 'font-family:monaco,sans-serif; font-size:12px; padding:10px; background-color:black; color:white;'
+			document.body.appendChild(opts.outputEl)
 		}
-		tinytest.outputEl.appendChild(document.createElement('div')).innerHTML = msg.replace(/\n/g, '<br/>')
+		var messageHTML = args.join(' ').replace(/\n/g, '<br/>')
+		opts.outputEl.appendChild(document.createElement('div')).innerHTML = messageHTML
+		
+		// Remove HTML from args for console.log
+		for (var key in args) {
+			args[key] = colour.strip(args[key])
+		}
 	}
+	
+	// Then, print to console
+	console.log.apply(console, args)
 }
 
 function noConflict() {
@@ -52,6 +81,12 @@ var globals = {
 	assert: assert,
 	test: test,
 	print: print
+}
+
+var opts = {
+	failFast: false,
+	outputEl: null,
+	maxDuration: 150
 }
 
 var global = (function() { return this })();
@@ -92,6 +127,7 @@ var yellow = colour.yellow
 var red = colour.red
 
 var runner = {
+	currentGroup: '',
 	tests: [],
 	failedTests: [],
 	current: null,
@@ -110,31 +146,42 @@ var runner = {
 		runner.current.t0 = new Date()
 		runner.failTimeout = setTimeout(function() {
 			runner._onTestDone('Test timed out')
-		}, tinytest.maxDuration)
-		if (runner.current.fn.length == 1) {
-			runner.current.fn(function(err) {
-				runner._onTestDone(err)
-			})
-			return
-		}
+		}, opts.maxDuration)
 		
-		var res = runner.current.fn()
-		if (res) {
-			res.then(function() {
-				runner._onTestDone(null)
-			}).catch(function(err) {
-				runner._onTestDone(err)
-			})
-			return
+		runner._runTest(runner.current.fn)
+	},
+	
+	_runTest: function(testFn) {
+		try {
+			if (runner.current.fn.length == 1) {
+				runner.current.fn(function(err) {
+					runner._onTestDone(err)
+				})
+
+			} else {
+				var promise = runner.current.fn()
+				if (promise) {
+					promise.then(function() {
+						runner._onTestDone(null)
+					}).catch(function(err) {
+						runner._onTestDone(err)
+					})
+
+				} else {
+					runner._onTestDone()
+				}
+			}
+
+		} catch(err) {
+			runner._onTestDone(err)
 		}
-		
-		runner._onTestDone(null)
 	},
 	
 	_onTestDone: function(err) {
 		if (!runner.current) {
 			print(red('Error during tests setup:'), '\n', err.stack ? err.stack : err.toString())
 			_exit(1)
+			return
 		}
 		clearTimeout(runner.failTimeout)
 		var duration = new Date() - runner.current.t0
@@ -145,6 +192,10 @@ var runner = {
 			runner.current.message = message
 			runner.current.duration = duration
 			print(red('Fail ' + duration+'ms'), '\n', message)
+			if (opts.failFast) {
+				runner._finish()
+				return
+			}
 		} else {
 			runner.current.result = true
 			runner.current.duration = duration
@@ -155,24 +206,32 @@ var runner = {
 	},
 	
 	_finish: function() {
+		var numSkipped = (runner.tests.length - runner.testIndex)
+
 		for (var i = 0; i < runner.tests.length; i++) {
 			if (!runner.tests[i].result) {
 				runner.failedTests.push(runner.tests[i])
 			}
 		}
 		
+		if (numSkipped) {
+			print(yellow(numSkipped+' tests skipped'))
+		}
 		if (runner.failedTests.length) {
 			print(red(runner.failedTests.length + ' tests failed:'))
 			for (var i = 0; i < runner.failedTests.length; i++) {
 				print(red('\t'+runner.failedTests[i].name))
 			}
+			_exit(1)
+			
 		} else if (runner.tests.length == 0) {
 			print(yellow('No tests found.'))
+			_exit(1)
+			
 		} else {
 			print(green('All done!'), runner.tests.length, 'tests passed.')
+			_exit(0)
 		}
-		
-		_exit(0)
 	}
 }
 
